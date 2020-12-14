14ccb32e66727378d2075fb7185fd8770e660b5b
Dicky Chan <hinyiuchan@hk01.com>
2020-12-14 17:03:43 +0800

    feat: user event. feat: sidebar height

---
+++ admin/src/components/LeftMenu/LeftMenuLinkSection/LeftMenuListLink.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const LeftMenuListLink = styled.div`
+  margin-bottom: 19px;
+  margin-right: 28px;
+  overflow: auto;
+`;
+
+export default LeftMenuListLink;
