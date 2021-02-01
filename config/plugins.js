084e1bdc31f1a2510a4d70965f8e23f008864228
SilWolf <silwolf1121@gmail.com>
2021-02-01 13:30:56 +0800

    chore: upgrade to 3.4.5

--- config/plugins.js
+++ config/plugins.js
@@ -1,13 +1,17 @@
 module.exports = ({ env }) => ({
   upload: {
-    provider: "aws-s3",
+    provider: "s3-plus",
     providerOptions: {
       accessKeyId: env("AWS_ACCESS_KEY_ID"),
-      secretAccessKey: env("AWS_ACCESS_SECRET"),
-      region: "ap-southeast-1",
+      secretAccessKey: env("AWS_SECRET_ACCESS_KEY"),
+      region: env("AWS_REGION"),
+      endpoint: env("AWS_S3_ENDPOINT"),
       params: {
-        Bucket: "hkirs-tvp-strapi-bucket",
+        Bucket: env("AWS_S3_BUCKET_NAME"),
+        ACL: "public-read",
+        // folder: 'myapp/images',
       },
+      s3ForcePathStyle: true, //needed for localstack s3 to work correctly
     },
   },
 });
