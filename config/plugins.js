ad7d455b367e17150bc5e4b640d7ed874c373d24
SilWolf <silwolf1121@gmail.com>
2021-03-03 14:12:14 +0800

    fix: s3 upload issue

--- config/plugins.js
+++ config/plugins.js
@@ -5,7 +5,7 @@ module.exports = ({ env }) => ({
       accessKeyId: env("AWS_ACCESS_KEY_ID"),
       secretAccessKey: env("AWS_SECRET_ACCESS_KEY"),
       region: env("AWS_REGION"),
-      endpoint: env("AWS_S3_ENDPOINT"),
+      endpoint: env("AWS_S3_ENDPOINT") || undefined,
       params: {
         Bucket: env("AWS_S3_BUCKET_NAME"),
         ACL: "public-read",
