b4b5d56523764490f526f5df98d04cfd49882ddb
SilWolf <silwolf1121@gmail.com>
2021-03-03 14:03:42 +0800

    feat: remove dashboard. add external links in menu

---
+++ admin/src/containers/HomePage/index.js
@@ -0,0 +1,17 @@
+import React, { memo } from "react";
+
+import { Block, Container } from "./components";
+
+const HomePage = ({ global: { plugins }, history: { push } }) => {
+  return (
+    <>
+      <Container className="container-fluid">
+        <div className="row">
+          <div className="col-12"></div>
+        </div>
+      </Container>
+    </>
+  );
+};
+
+export default memo(HomePage);
